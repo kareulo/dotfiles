c04768042022d128a41cc48f25d52c46f0978740
kareulo <jcajuab+github@proton.me>
2024-10-21 22:53:41 +0800

    feat(fish): update config
    - Add ~/.local/bin to PATH
    - Add mise
    - Add comments

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
+    # Add ~/.local/bin to PATH
+    fish_add_path -m ~/.local/bin
+
+    # Starship
     starship init fish | source
     enable_transience
+
+    # Mise
+    mise activate fish | source
 end
