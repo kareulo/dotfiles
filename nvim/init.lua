2cde75bfa67da655e31c6d43c7138c3670ce63cd
kareulo <jcajuab@tutamail.com>
2024-12-23 11:10:18 +0800

    feat: update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("user.options")
 
 if vim.g.vscode then
     require("user.vscode.keymaps")
+    require("user.vscode.options")
 else
     require("user.neovim.options")
 end
--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -6,6 +6,3 @@ vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
 vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
 
 vim.keymap.set("v", "p", '"_dp', { noremap = true, silent = true })
-
-vim.keymap.set({ "n", "v" }, "<Leader>y", '"+y', { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<Leader>p", '"+p', { noremap = true, silent = true })
