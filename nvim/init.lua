eeae3a28943cde945affba17ae0d404374ba2d1e
kareulo <jcajuab@tutamail.com>
2024-12-27 17:43:39 +0800

    feat: add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.markdown_recommended_style = 0
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.colorcolumn = "80"
+vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.cursorline = true
+vim.opt.formatoptions = "jcroqlnt"
+vim.opt.ignorecase = true
+vim.opt.inccommand = "nosplit"
+vim.opt.jumpoptions = "view"
+vim.opt.laststatus = 3
+vim.opt.linebreak = true
+vim.opt.list = true
+vim.opt.mouse = "a"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ruler = false
+vim.opt.scrolloff = 999
+vim.opt.shiftround = true
+vim.opt.shortmess:append({ W = true, I = true, C = true, c = true })
+vim.opt.signcolumn = "yes"
+vim.opt.spelllang = { "en" }
+vim.opt.splitbelow = true
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+vim.opt.timeoutlen = 200
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wrap = false
+
+if vim.fn.has("nvim-0.10") == 1 then
+    vim.opt.smoothscroll = true
+end
